8ed562f71992cc48cb6ca90cab4c7c04e2c6b337
patryk-siewiera <patryk.siewiera@gmail.com>
2020-05-29 11:58:37 +0200

    finished video desktop script

--- ydl_video_clipboard.py
+++ ydl_video_clipboard.py
@@ -4,6 +4,10 @@ import youtube_dl
 import time
 
 
+def press_to_continue():
+    input("\n\nPress Enter to Exit...\n")
+
+
 def clipboard_validate_url():
     clipboard = pyperclip.paste()
     validators_message = "\nUrl validation check: \t\t"
@@ -16,18 +20,20 @@ def clipboard_validate_url():
     else:
         print(validators_message, validations_bool)
         print("\n>> WRONG LINK <<\nEXIT")
-        time.sleep(2)
+        press_to_continue()
         exit()
 
 
 def ydl_download(link):
     ydl_opts = {
-        'outtmpl': 'C:/Users/workp/Desktop/%(title)s.%(ext)s',
+        'outtmpl': 'C:/Users/workp/Desktop/%(title)s -- %(uploader)s.%(ext)s',
         'noplaylist': True,
     }
 
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         ydl.download([link])
+
+    press_to_continue()
 
 
 ydl_download(clipboard_validate_url())
