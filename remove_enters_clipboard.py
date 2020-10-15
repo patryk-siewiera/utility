8647e89b43277ff5a64f08ee292659995af955f8
psiewiera <patryk.siewiera@gmail.com>
2020-10-15 13:36:50 +0200

    remove Ŝ -> z

--- remove_enters_clipboard.py
+++ remove_enters_clipboard.py
@@ -8,7 +8,9 @@ print("\t\t***\t\tclipboard before\n\n", clp, "\n\n")
 
 clp = clp.splitlines()
 clp = " ".join(clp)
+clp = clp.replace('Ŝ','z')
 
 print("\t\t###\t\tclipboard AFTER\n\n", clp, "\n\n")
 
 pyperclip.copy(clp)
+
